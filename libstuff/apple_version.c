7ef2db814d58116b85972566985fdc0a4eb71f2b
DeaDDooMER <deaddoomer@deadsoftware.ru>
2022-07-09 22:24:13 +0300

    init: odcctools 2006/04/13

---
+++ libstuff/apple_version.c
@@ -0,0 +1,6 @@
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+const char apple_version[]= PACKAGE_NAME "-" PACKAGE_VERSION;
+const char ld64VersionString[]= "@(#)PROGRAM:ld64  PROJECT:" PACKAGE_NAME "-" PACKAGE_VERSION "\n";
